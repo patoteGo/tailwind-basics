da362d5aeabe05291fae64f2095a73e283d6fda0
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-11-21 17:04:21 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    // enabled: true,
+    // content: ['./dist/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
